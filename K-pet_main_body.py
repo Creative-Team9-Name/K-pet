a126fded72d9f96cda03c2253710db8ae6c1a3e1
CottonCandy01 <angelahu2001@gmail.com>
2024-05-31 07:58:49 +0200

    bt works fine

--- K-pet_main_body.py
+++ K-pet_main_body.py
@@ -8,29 +8,108 @@ hub = PrimeHub()
 
 hub.light_matrix.show_image('HAPPY')
 
-def init():
-    cat_motor = Motor('A')
-    goose_motor = Motor('B')
-    duck_motor = Motor('C')
-    # initialize remaining sensors
-    # TODO: initialize MotorPair
+animal = -1
 
-class BT():
+
+# message setting:
+# 1. 2 byte for animal type
+# 2. 6 bytes for position
+
+class FoodDispenser():
+
     def __init__(self):
-        self.com  = BT_VCP(0)
+        self.cat_motor = Motor('A')
+        self.duck_motor = Motor('C')
+        self.goose_motor = Motor('D')
+        self.cat_motor.run_to_position(degrees=90)
+        self.cat_motor.run_to_position(degrees=90)
+        self.goose_motor.run_to_position(degrees=270)
+        self.goose_motor.run_to_position(degrees=270)
+        self.duck_motor.run_to_position(degrees=0)
+        self.duck_motor.run_to_position(degrees=0)
+        self.cat_rotation = True
+        self.duck_rotation = True
+        self.goose_rotation = True
+
+    def serve_cat_food(self):
+        global hub
+        if self.cat_rotation:
+            try:
+                self.cat_motor.run_for_rotations(0.5)
+            except Exception as e:
+                print('Error: ', e)
+                hub.light_matrix.show_image('SAD')
+                # self.cat_motor.run_to_position(degrees=90)
+            finally:
+                self.cat_rotation = False
+        else:
+            try:
+                self.cat_motor.run_for_rotations(-0.5)
+            except Exception as e:
+                print('Error: ', e)
+                hub.light_matrix.show_image('SAD')
+            finally:
+                self.cat_rotation = True
+
+    def serve_goose_food(self):
+        global hub
+        if self.goose_rotation:
+            try:
+                self.goose_motor.run_to_degrees_counted(degrees=450)
+            except Exception as e:
+                print('Error: ', e)
+                hub.light_matrix.show_image('SAD')
+            finally:
+                self.goose_rotation = False
+        else:
+            try:
+                self.goose_motor.run_to_degrees_counted(degrees=90)
+            except Exception as e:
+                print('Error: ', e)
+                hub.light_matrix.show_image('SAD')
+            finally:
+                self.goose_rotation = True
+
+
+    def serve_duck_food(self):
+        global hub
+        if self.duck_rotation:
+            try:
+                self.duck_motor.run_to_position(degrees=270)
+            except Exception as e:
+                print('Error: ', e)
+                hub.light_matrix.show_image('SAD')
+            finally:
+                self.duck_rotation = False
+        else:
+            try:
+                self.duck_motor.run_to_position(degrees=90)
+            except Exception as e:
+                print('Error: ', e)
+                hub.light_matrix.show_image('SAD')
+            finally:
+                self.duck_rotation = True
+
+
+class Bluetooth():
+
+    def connection_status():
+        if not self.com.isconnected():
+            hub.status_light.off('green')
+            hub.status_light.on('red')
+
+    def __init__(self):
+        print('hello')
+        self.com= BT_VCP(0)
         while not self.com.isconnected():
             hub.status_light.on('red')
-        hub.status_light.off('red')
         hub.status_light.on('green')
-        self.com.setinterrupt(-1) # TODO: functionality not clear
-        
-        def connection_status():
-            if not self.com.isconnected():
-                hub.status_light.off('green')
-                hub.status_light.on('red')
+        time.sleep(5)
+        hub.status_light.off()
+        # self.com.setinterrupt(-1) # TODO: functionality not clear
 
-        self.com.callback(self.connection_status())
-    
+        # self.com.callback() #self.connection_status()
+
     def connect(self):
         count = 0
         while not hub.left_button.is_pressed():
@@ -38,14 +117,18 @@ class BT():
             if not received_bytes: continue
             received_data = received_bytes.decode()
             print("Received: ", received_data)
-
-            data_to_send = "Bye{}".format(count)
+            animal = int(received_data)
+            
+            # send to rasberry
+            data_to_send = "ReceivedMSG{}".format(count)
             send_bytes = self.com.send(data_to_send.encode('utf-8'), timeout=100)
             count += 1
-
             print("Send: ", data_to_send)
+            return animal
 
-    def any(self): # what does this function do?   
+        return -1
+
+    def any(self): # what does this function do?
         return self.com.any()
 
     def read(self, msg):
@@ -55,21 +138,21 @@ class BT():
         self.com.close()
 
 
-# TODO: modify move class 
+# TODO: modify move class
 class Move(): # class to move the robot
 
     def __init__(self): # front_wheels = (right, left)
         self.motor = MotorPair('F', 'E')
         self.distance = DistanceSensor('B')
         self.motor.set_default_speed(50) # check if correct
-    
-    def check_obstacle(self)->bool: # Obstacle: True  No obstacle: False
+
+    def check_obstacle(self)->bool: # Obstacle: TrueNo obstacle: False
         obstacle_distance = self.distance.get_distance_cm()
         if obstacle_distance != None and obstacle_distance < 30:
             return True
         return False
-    
-    def move_straight(self, distance):        
+
+    def move_straight(self, distance):
         #self.motor.move_tank(amount = distance , unit='cm', left_speed=-50, right_speed=-50) # check if direction is correct
         self.motor.start()
         count = 0
@@ -104,36 +187,48 @@ class Move(): # class to move the robot
     def avoid_obstacle(): # future upgrade
         pass
 
-class FoodDispenser():
-    
-    def __init__(self):
-        self.cat_motor = Motor('D')
-        self.goose_motor = Motor('C')
-        self.duck_motor = Motor('A')
-    
-    def serve_cat_food(self):
-        self.cat_motor.run_for_rotations(0.5)
-    
-    def serve_goose_food(self):
-        self.goose_motor.run_for_rotations(0.5)
-
-    def serve_duck_food(self):
-        self.duck_motor.run_for_rotations(0.5)
 
 
-bt = BT()
+bt = Bluetooth()
 food = FoodDispenser()
+movement = Move()
+
+hub.left_button.wait_until_pressed()
+hub.speaker.beep(seconds=1)
+while True:
+    msg = bt.connect()
+    if msg >= 0 and msg <3:
+        if msg == 0:
+            # reach animal first
+            movement.move_straight(10)
+            food.serve_cat_food()
+            print('serving cat food')
+        elif msg == 1:
+            # reach animal first
+            movement.move_straight(10)
+            food.serve_duck_food()
+            print('serving duck food')
+        elif msg == 2:
+            # reach animal first
+            movement.move_straight(10)
+            food.serve_goose_food()
+            print('serving goose food')
+    elif msg >= 3 or msg < 0:
+        hub.light_matrix.show_image('SAD')
+        break
+
+'''
 
 msg = []
 while not hub.left_button.is_pressed():
     if bt.any():
         readinto(msg, 2)
-
-        
-
+'''
 
 
-    
 
-    
+
+
+
+
 
